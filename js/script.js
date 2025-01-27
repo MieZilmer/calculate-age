6d5f16418f98d48dc301ad42dbd87487bc69d3c0
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-01-27 09:49:01 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+
+const birthDateString = prompt("What is your birth date? (YYYY-MM-DD)");
+const birthDate = new Date(birthDateString);
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+let age = currentYear - birthDate.getFullYear();
+
+// juster alder efter om datoen er passeret i år
+if (currentDate < new Date(currentYear, birthDate.getMonth(), birthDate.getDate())) {
+    age--;
+}
+
+console.log(`You are turning ${age + 1} years old this year.`);
